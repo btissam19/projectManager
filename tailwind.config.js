a88cbe8cabad7e2e781ef65a780e0bea4b125338
Ibtissam <btissam1234btissam12@gmail.com>
2023-09-30 17:39:36 +0100

    message form 2

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/**/*.hbs',   // For Handlebars templates
+    './public/JS/**/*.js', // For Jav
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
